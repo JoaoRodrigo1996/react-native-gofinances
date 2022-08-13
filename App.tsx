4b0a32b2fcfb6aa25eb790070d7cddae8a24934a
Rodrigo Mesquita <rodrigo.mesquita96@hotmail.com>
2022-08-13 00:31:56 -0300

    Finalizando listagem de transações

---
+++ App.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
+
+import { defaultTheme } from "./src/global/styles/theme";
+
+import { Dashboard } from "./src/screens/Dashboard";
+
+import * as SplashScreen from "expo-splash-screen";
+
+export default function App() {
+  SplashScreen.preventAutoHideAsync();
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  SplashScreen.hideAsync();
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Dashboard />
+    </ThemeProvider>
+  );
+}
